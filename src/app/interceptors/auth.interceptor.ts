16b51dc17df134859e342d3e268caba7c38cfda9
Gerijacki <gerardloriz05@gmail.com>
2026-01-17 22:23:53 +0100

    feat: New backend prod domain

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,9 +1,32 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const authReq = req.clone({
+  const token = getCookie('XSRF-TOKEN');
+
+  let authReq = req.clone({
     withCredentials: true,
   });
 
+  if (token) {
+    authReq = authReq.clone({
+      headers: authReq.headers.set('X-XSRF-TOKEN', token),
+    });
+  }
+
   return next(authReq);
 };
+
+function getCookie(name: string): string | null {
+  const nameLenPlus = name.length + 1;
+  return (
+    document.cookie
+      .split(';')
+      .map((c) => c.trim())
+      .filter((cookie) => {
+        return cookie.substring(0, nameLenPlus) === `${name}=`;
+      })
+      .map((cookie) => {
+        return decodeURIComponent(cookie.substring(nameLenPlus));
+      })[0] || null
+  );
+}
